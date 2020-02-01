1868e787d07c01f0fc2355278fbad8b8fb476865
Facundo <facuaaraujo@gmail.com>
2020-02-01 19:37:14 -0300

    Add Javascript - Fetch API

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Obteniendo lista de paises del DOM
+const countriesList = document.querySelector('.countries-list');
+
+// Petición a la API
+fetch('https://restcountries.eu/rest/v2/all')
+    .then(response => response.json())
+    .then(function(countries){
+        console.log(countries);        
+        
+        //Recorrer array con los paises 
+        countries.forEach(country => {            
+            // Template String
+            var countryCard = `
+                                <article class="country">
+                                <div class="flag"><img src="${country.flag}" alt="${country.name}"></div>
+                                <div class="name">${country.name}</div>
+                                <div class="population"><span>Population: </span>${country.population}</div>
+                                <div class="region"><span>Region: </span>${country.region}</div>
+                                <div class="capital"><span>Capital: </span>${country.capital}</div>
+                                </article>
+                                `;
+
+            // Insertando Template en la lista de paises
+            countriesList.insertAdjacentHTML('beforeend', countryCard);
+        });
+    });
+
