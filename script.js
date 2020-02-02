8f262618da2bceb8b5b5fa2683a38ee9e140555e
Facundo <facuaaraujo@gmail.com>
2020-02-02 01:53:40 -0300

    Add search inputs and filter by region funcionality

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
     .then(function(countries){ 
         //Recorrer array con los paises 
+        var countries = countries;
         countries.forEach(country => {            
             // Template String
             var countryCard = `
@@ -47,4 +48,32 @@ toggleButton.addEventListener('click', function(){
     if(body.classList.value === 'dark'){
         moonImage.setAttribute('src', './img/moon-solid.svg');
     }else{ moonImage.setAttribute('src', './img/moon-regular.svg'); }
+});
+
+
+// FILTER SELECT
+
+// Obteniendo select del DOM
+const regionSelect = document.getElementById('filter-region');
+
+// Capturando evento change del select
+regionSelect.addEventListener('change', (event) => {
+    // Obteniendo todos los paises del DOM
+    const countries = document.getElementsByClassName('country');
+    // Variable con la region seleccionada desde el select
+    var region = event.srcElement.value.toLocaleLowerCase();
+
+    // Recorriendo todos los paises del DOM
+    for (let i = 0; i < countries.length; i++) {
+        const el = countries[i];
+        // Obteniendo region de cada pais
+        const elRegion = el.querySelector('.region').innerHTML.toLocaleLowerCase();        
+        
+        // Comprobar si coincide con la region seleccionada
+        if(!elRegion.includes(region) && region !== 'filter by region'){
+            el.style.display = "none";
+        }else {
+            el.style.display = "block";
+        }
+    }
 });
